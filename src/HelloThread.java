f2b4ecdc09d7c4072505e58c35d154f48a7b786e
John Berkley <jberkley95@gmail.com>
2018-10-01 16:49:58 -0700

    Lecture 3 Code added

---
+++ src/HelloThread.java
@@ -0,0 +1,14 @@
+/**
+ * @author John Berkley
+ * CPP Class: <Insert Class Here>
+ * Date Created: Oct 01, 2018
+ */
+public class HelloThread extends Thread {
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String args[]) {
+        (new HelloThread()).start();
+    }
+}
---
+++ src/SleepMessages.java
@@ -0,0 +1,44 @@
+/**
+ * @author John Berkley
+ * CPP Class: <Insert Class Here>
+ * Date Created: Oct 01, 2018
+ */
+public class SleepMessages {
+
+    public static void main(String args[])
+
+            throws InterruptedException {
+
+        String importantInfo[] = {
+
+                "Mares eat oats",
+
+                "Does eat oats",
+
+                "Little lambs eat ivy",
+
+                "A kid will eat ivy too"
+
+        };
+
+
+
+        for (int i = 0;
+
+             i < importantInfo.length;
+
+             i++) {
+
+            //Pause for 4 seconds
+
+            Thread.sleep(4000);
+
+            //Print a message
+
+            System.out.println(importantInfo[i]);
+
+        }
+
+    }
+
+}
